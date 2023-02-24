dab1f021652176c1a8dab3ff91ba6f98539dcb96
oroola <adedoyin2@gmail.com>
2023-02-24 06:21:09 +0000

    functions nested loop file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *  * print_sign - prints the sign of a number
  *   * @n: the int to check
@@ -10,15 +11,15 @@ int print_sign(int n)
 {
 		if (n > 0)
 				{
-							_putchar('+');
+							_putchar(43);
 									return (1);
-										} else if (n == 0)
+										} else if (n < 0)
+										{
+													_putchar(45);
+															return (-1);
+																} else
+																		{
+																					_putchar(48);
+																							return (0);
+																								}																	_putchar('\n');
 }
-		_putchar(48);
-				return (0);
-					} else if (n < 0)
-	{
-				_putchar('-');
-					}
-		return (-1);
-		}
