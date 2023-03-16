70fb94b8fbf6da10e356b06fa187701b55f01608
oroola <adedoyin2@gmail.com>
2023-03-16 05:03:08 +0000

    0x0B. C - malloc, free files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,10 +5,8 @@
  * @s1: input one to concat
  * @s2: input two to concat
  * Return: concat of s1 and s2
-*/
-
+ */
 char *str_concat(char *s1, char *s2)
-
 {
 	char *conct;
 	int i, ci;
@@ -17,25 +15,29 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
+
+	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
+
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
-	conct[i] = s1[i];
+		conct[i] = s1[i];
 		i++;
 	}
+
 	while (s2[ci] != '\0')
 	{
-	conct[i] = s2[ci];
+		conct[i] = s2[ci];
 		i++, ci++;
 	}
 	conct[i] = '\0';
 	return (conct);
 }
+
